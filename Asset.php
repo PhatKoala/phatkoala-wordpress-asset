689b991e4b5fe5e6abfd59b695576d6774919642
Stewart Walter <stewart@phatkoala.uk>
2021-10-28 17:09:17 +0100

    Docblocks and PHPCS

--- Asset.php
+++ Asset.php
@@ -4,30 +4,56 @@ declare(strict_types=1);
 
 namespace PhatKoala;
 
+/**
+ * Class Asset
+ * @package PhatKoala
+ */
 class Asset
 {
+    /**
+     * @var array|null
+     */
     private static $manifest;
 
+    /**
+     * @var array|null
+     */
     private static $entrypoints;
 
+    /**
+     * @return array
+     */
     public static function getManifest(): array
     {
         if (is_null(self::$manifest)) {
-            self::$manifest = json_decode(file_get_contents(sprintf('%s/build/manifest.json', getcwd())), true);;
+            self::$manifest = json_decode(
+                file_get_contents(sprintf('%s/build/manifest.json', getcwd())),
+                true
+            );
         }
 
         return self::$manifest;
     }
 
+    /**
+     * @return array
+     */
     public static function getEntrypoints(): array
     {
         if (is_null(self::$entrypoints)) {
-            self::$entrypoints = json_decode(file_get_contents(sprintf('%s/build/entrypoints.json', getcwd())), true);;
+            self::$entrypoints = json_decode(
+                file_get_contents(sprintf('%s/build/entrypoints.json', getcwd())),
+                true
+            );
         }
 
         return self::$entrypoints;
     }
 
+    /**
+     * @param string $file
+     * @return string|null
+     */
     public static function file(string $file): ?string
     {
         $manifest = self::getManifest();
@@ -39,6 +65,10 @@ class Asset
         return null;
     }
 
+    /**
+     * @param string $name
+     * @return array|null
+     */
     public static function entrypoint(string $name): ?array
     {
         $entrypoints = self::getEntrypoints();
